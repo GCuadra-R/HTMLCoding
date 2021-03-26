0d79dcbfda3204622ed369a91a2874b99c79a640
GCuadra-R <78924555+GCuadra-R@users.noreply.github.com>
2021-03-26 20:03:34 +0100

    Disaster today

---
+++ Combinados/arrays.js
@@ -0,0 +1,20 @@
+/*
+var x=10
+var z=20
+document.writeln(x)
+document.writeln(x+z)
+//Crear un array
+var array1=[1,90,10]
+document.writeln(array1)
+//Acceder a un elemento del array
+document.writeln(array1[0])
+//crear un array con el operador new
+var personas=new Array("Pedro", "Antonio", "Rosa")
+document.writeln(personas)
+*/
+
+var array1=[24, 68, 32]
+var personas=new Array("Pedro", "Antonio", "Rosa")
+document.getElementById("caja1").innerHTML= array1
+document.getElementById("caja2").innerHTML= personas
+document.getElementById("caja3").innerHTML= personas[1]
--- Combinados/funciones.js
+++ Combinados/funciones.js
@@ -1,18 +1,12 @@
-function saludar()
-{
-    return "Hola"
+function sumar(){
+
+    var numero1=document.getElementById("num1").value
+    var numero2=document.getElementById("num2").value
+
+    var numero1_valido=parseInt(numero1);
+    var numero2_valido=parseInt(numero2);
+    var resultado = numero1_valido+numero2_valido
+
+    document.getElementById("resultado1").innerHTML= resultado
+    
 }
-
-var a=8
-var b=5
-
-function sumar(a,b)
-{
-    return a+b
-}
-
-var x=10
-var z=20
-var resultado=sumar(x,z)
-document.write(resultado)
-document.write(saludar())
